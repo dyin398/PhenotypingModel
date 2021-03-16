6a5aaeeae0f83591e0535924715231b689ec61a3
dyin398 <47566078+dyin398@users.noreply.github.com>
2021-03-16 00:03:37 +0000

    Add files via upload

--- ProjectCode/src/dataPrep/tableExtractors/extractor.py
+++ ProjectCode/src/dataPrep/tableExtractors/extractor.py
@@ -1,6 +1,8 @@
 import abc
 from abc import ABC, abstractmethod
 
+# Abstract class containing potential methods for data preparation. Includes
+# a super method which takes a dataframe and outputs it in suitable form
 class Extractor(ABC):
 
     # Initialise class with data and outcomes
@@ -16,7 +18,37 @@ class Extractor(ABC):
     def manuallyRemoveFeatures(self):
         pass
     
+    # Abstract method to clean data selected for parsing
+    def cleanData(self):
+        pass
+
+    # Abstract method to transform data into something that can be computed
+    def transformData(self):
+        pass
+
     # Abstract method to use different preparation methods to 
     # optimise remaining data values.
     def featureSelection(self):
         pass
+
+    # Abstract method which returns data for main to impute if values are missing
+    def returnImputeData(self):
+        pass
+
+    # Method turning dataframe into a dictionary of patient to array of features
+    # and a list of feature names
+    def dataFrameToDict(self, data):
+        patients = data['PATIENT']
+        data.drop(['PATIENT'], axis=1, inplace=True)
+        features = data
+        patientsList = patients.values.tolist()
+        featuresList = features.values.tolist()
+        featuresDict = {}
+        featureNames = list(data.columns.values) 
+
+        for i in range(len(patientsList)):
+            patient = patientsList[i]
+            feature = featuresList[i]
+            featuresDict[patient] = feature
+        
+        return featuresDict, featureNames
--- ProjectCode/src/dataPrep/tableExtractors/patients_extractor.py
+++ ProjectCode/src/dataPrep/tableExtractors/patients_extractor.py
@@ -1,4 +1,8 @@
-from .extractor import Extractor
+from dataPrep.tableExtractors.extractor import Extractor
+from utils.data_prep_methods import PCAalgorithm, standardiseData
+from datetime import datetime
+import pandas as pd
+import statistics
 
 # Extractor for the patient's .csv
 class PatientsExtractor(Extractor):
@@ -8,5 +12,68 @@ class PatientsExtractor(Extractor):
         self.outcomes = outcomes
 
     def extractFeatures(self):
-        
-        return [], []
+        self.manuallyRemoveFeatures()
+        self.cleanData()
+        self.transformData()
+        self.featureSelection()
+        return super().dataFrameToDict(self.data), self.returnImputeData()
+
+    def manuallyRemoveFeatures(self):
+        features_to_keep = ['Id', 'BIRTHDATE', 'MARITAL', 'RACE', 'GENDER', 'CITY']
+        self.data = self.data[features_to_keep]
+
+    # Function which cleans the data we've decided to keep for easier manipulation
+    def cleanData(self):
+        self.imputeData()
+
+    def transformData(self):
+        self.data['BIRTHDATE'] = self.data['BIRTHDATE'].apply(self.getAge)
+        self.data['MARITAL'] = self.data['MARITAL'].apply(self.isMarried)
+        self.data['GENDER'] = self.data['GENDER'].apply(self.isMale)
+        self.data.rename({'BIRTHDATE': 'AGE', 'MARITAL': 'MARRIED', 'GENDER': 'MALE', 'Id': 'PATIENT'}, axis=1, inplace=True)
+    
+    def featureSelection(self):
+        self.data.drop(['RACE', 'CITY'], axis=1, inplace=True)
+        features = ['AGE', 'MARRIED', 'MALE']
+        newData = self.reduceDimensionality(self.data[features])
+        self.data.drop(features, axis=1, inplace=True)
+        self.data = pd.concat([self.data, newData], axis=1)
+
+    def imputeData(self):
+        self.data['MARITAL'] = self.data['MARITAL'].fillna('S')
+
+    def reduceDimensionality(self, data):
+        standardised_features = standardiseData(data)
+        components = PCAalgorithm(standardised_features, 1)
+        newData = pd.DataFrame(data = components, columns = ['SOCIAL_GROUP'])
+        return newData
+
+    def returnImputeData(self):
+        output = []
+        socialGroup = self.data['SOCIAL_GROUP']
+        groupList = socialGroup.values.tolist()
+        output.append(statistics.median(groupList))
+        return output
+    
+    def getAge(self, date):
+        if isinstance(date, int):
+            return date
+        curryear = datetime.now().year
+        birthyear = datetime.strptime(date, '%Y-%m-%d').year
+        return curryear-birthyear
+
+    def isMarried(self, status):
+        if status == 'S':
+            return 0
+        elif status == 'M':
+            return 1
+        else:
+            return status
+
+    def isMale(self, gender):
+        if gender == 'F':
+            return 0
+        elif gender == 'M':
+            return 1
+        else:
+            return gender
