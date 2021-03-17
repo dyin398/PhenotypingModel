3bccecd04428d76aaf8fc31a8052e355e324dfa8
David Yin <k1891137>
2021-03-17 22:13:26 +0000

    Add testing for classifiers

--- ProjectCode/src/main.py
+++ ProjectCode/src/main.py
@@ -1,4 +1,9 @@
 from dataPrep.data_preparation import DataWrangler
+from utils.classifiers import logisticRegression, getClassificationReport, getConfusionMatrix, getAccuracyScore
 
 wrangler = DataWrangler()
 patient_features, patient_outcomes = wrangler.wrangleData()
+y_test, predictions = logisticRegression(patient_features, patient_outcomes)
+print(getClassificationReport(y_test, predictions))
+print(getConfusionMatrix(y_test, predictions))
+print(getAccuracyScore(y_test, predictions))
